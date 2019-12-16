2befc7a2248ebc561a63c8245bae114a79d7c482
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-16 18:59:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(phrase)
   final_phrase = []
   array_phrase = phrase.split
-  another_phrase= array_phrase.reverse
-  
+  another_phrase= array_phrase.each
+    
   
 end  
   
